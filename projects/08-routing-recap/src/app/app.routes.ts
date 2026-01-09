a5a299bf36d4ba602ad6005b21ba1f4132f76f07
ABOUFATAMA Hamza <hamzaaboufatama@gmail.com>
2026-01-09 12:22:59 +0100

    feat: Enhance routing and details component with dynamic data binding

--- projects/08-routing-recap/src/app/app.routes.ts
+++ projects/08-routing-recap/src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent 
+    }
+];
